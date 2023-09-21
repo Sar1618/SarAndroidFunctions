0d3774fd608f708e7d48dee8f1451852bc15fc95
Sar <sar1618@126.com>
2023-09-21 17:31:52 +0800

    工作日志合并时加些判断条件；

--- app/src/main/java/com/sar/sarandroidfunctions/FileMerge.kt
+++ app/src/main/java/com/sar/sarandroidfunctions/FileMerge.kt
@@ -8,16 +8,18 @@ fun main() {
     val listFiles = File("C:\\Users\\Administrator\\Desktop\\20221126").listFiles()
     val file = File("C:\\Users\\Administrator\\Desktop\\merge.txt")
     file.createNewFile()
+    val strings = mutableListOf<String>()
     FileWriter(file).use { rw ->
         listFiles.forEach {
             FileReader(it).use { fr ->
                 fr.readLines().forEach { text ->
-                    rw.write(text)
-                    rw.write("\r\n")
+                    if (!strings.contains(text)) {
+                        rw.write(text)
+                        strings.add(text)
+                        rw.write("\r\n")
+                    }
                 }
             }
-            rw.write("\r\n")
-            rw.write("\r\n")
         }
     }
 }
